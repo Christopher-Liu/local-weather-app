d20f376ab5bbe0cf6b37124349f03091bcd106fe
Christopher-Liu <christopherbrianliu@gmail.com>
2017-06-18 20:47:09 -0700

    Made the JavaScript in async-style
    -Instead of having two separate AJAX requests, one after the other, with
    async: set to false, I put the second request inside the callback
    function of the first, as per asynchronous styled programming

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-  var ipAPI = "https://freegeoip.net/json/";
   var longi;
   var lati;
   var location;
@@ -12,28 +10,29 @@ $(document).ready(function() {
 
   // API call to get location data from the IP API
   $.ajax({
-    url: ipAPI,
-    async: false,
+    url: "https://freegeoip.net/json/",
+    async: "false",
     dataType: "json",
     success: function(data) {
       longi = data.longitude;
       lati = data.latitude;
-  }});
 
-  $.ajax({
-    url: ("https://api.wunderground.com/api/7965a61d5be76ab2/conditions/q/" + lati + "," + longi + ".json"),
-    async: false,
-    dataType: "json",
-    success: function(response) {
-      location = response.current_observation.display_location.city;
-      temperatureF = response.current_observation.temp_f;
-      temperatureC = response.current_observation.temp_c;
-      weatherInfo = response.current_observation.weather;
-      weatherIcon = response.current_observation.icon_url;
-      $(".location").append(location);
-      $(".weather-info").append(weatherInfo);
-      $(".temperature-info").append(temperatureF +"°F");
-      $("#weather-icon").html("<img src=\"" + weatherIcon + "\">");
+      $.ajax({
+        url: "https://api.wunderground.com/api/7965a61d5be76ab2/conditions/q/" + lati + "," + longi + ".json",
+        dataType: "json",
+        success: function(response) {
+          console.log("https://api.wunderground.com/api/7965a61d5be76ab2/conditions/q/" + lati + "," + longi + ".json");
+          location = response.current_observation.display_location.city;
+          temperatureF = response.current_observation.temp_f;
+          temperatureC = response.current_observation.temp_c;
+          weatherInfo = response.current_observation.weather;
+          weatherIcon = response.current_observation.icon_url;
+          $(".location").append(location);
+          $(".weather-info").append(weatherInfo);
+          $(".temperature-info").append(temperatureF +"°F");
+          $("#weather-icon").html("<img src=\"" + weatherIcon + "\">");
+        }
+      });
     }
   });
 
