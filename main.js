bc15c33b57c553d806e7ad6f39fc75adce3acff0
Christopher-Liu <christopherbrianliu@gmail.com>
2017-06-21 11:24:20 -0700

    Introduced the forecast API and implemented the forecast bar
    - Web app now also uses the Wunderground forecast API to pull in
    forecast data for the following 3 days in addition to the current
    conditions (which comes from the conditions API)
    - A total of 3 AJAX calls are made to display the relevant information
    on the screen now; the IP API to get location data, the conditions API
    to get present weather data, and the forecast API to get weather
    prediction data
    - Re-did the layout of the web app to have two main sections; the main
    section with current conditions along with the second section/half with
    the weather forecast
    - All HTML is empty, acting as a template that will later be filled with
    the results of the AJAX calls
    
    To Do:
    - Need to have each forecast box be a different shade of blue (a
    gradient that gradually gets lighter). Thinking of doing this by setting
    the forecast-box's color with a JavaScript loop
    - Figure out why the current conditions temperature is also displaying a
    unicode character along with the temp
    - Finish implementing the layout

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
   var longi;
   var lati;
   var location;
-  var weatherInfo;
-  var weatherIcon;
   var temperatureF;
   var temperatureC;
   var token = "F";
+
+  var forecastF = [];
+  var forecastC = [];
 
   // API call to get location data from the IP API
   $.ajax({
@@ -14,23 +15,53 @@ $(document).ready(function() {
     async: "false",
     dataType: "json",
     success: function(data) {
+      console.log("IP AJAX request successful");
+
       longi = data.longitude;
       lati = data.latitude;
+      location = data.city;
 
+      $("#main-location").append(location);
+
+
+
+      // API call to get current weather conditions from Wunderground API
       $.ajax({
         url: "https://api.wunderground.com/api/7965a61d5be76ab2/conditions/q/" + lati + "," + longi + ".json",
         dataType: "json",
         success: function(response) {
-          console.log("https://api.wunderground.com/api/7965a61d5be76ab2/conditions/q/" + lati + "," + longi + ".json");
-          location = response.current_observation.display_location.city;
+          console.log("Weather conditions AJAX request successful");
+
           temperatureF = response.current_observation.temp_f;
           temperatureC = response.current_observation.temp_c;
-          weatherInfo = response.current_observation.weather;
-          weatherIcon = response.current_observation.icon_url;
-          $(".location").append(location);
-          $(".weather-info").append(weatherInfo);
-          $(".temperature-info").append(temperatureF +"°F");
-          $("#weather-icon").html("<img src=\"" + weatherIcon + "\">");
+
+          $("#main-temp").append(temperatureF + "°F");
+          $("#main-weather-info").append(response.current_observation.weather);
+          $("#main-weather-icon").html("<img src=\"" + response.current_observation.icon_url + "\">");
+        }
+      });
+
+
+      // API call to get forecast data from the Wunderground API
+      $.ajax({
+        url: "https://api.wunderground.com/api/7965a61d5be76ab2/forecast/q/" + lati + "," + longi + ".json",
+        dataType: "json",
+        success: function(res) {
+          console.log("Weather forecast AJAX request successful");
+
+          var forecastArray = res.forecast.simpleforecast.forecastday;
+          var counter = 0;
+
+          $("#forecast-section").children().each(function() {
+            $(this).children(".forecast-day").append(forecastArray[counter].date.weekday_short);
+            $(this).children(".forecast-icon").html("<img src=\"" + forecastArray[counter].icon_url + "\">");
+            $(this).children(".forecast-temp").append(forecastArray[counter].high.fahrenheit + "/" + forecastArray[counter].low.fahrenheit);
+            $(this).children(".forecast-info").append(forecastArray[counter].conditions);
+
+            forecastF.push(forecastArray[counter].high.fahrenheit + "/" + forecastArray[counter].low.fahrenheit);
+            forecastC.push(forecastArray[counter].high.celsius + "/" + forecastArray[counter].low.celsius);
+            counter ++;
+          });
         }
       });
     }
@@ -38,15 +69,14 @@ $(document).ready(function() {
 
 
 
-
-  $(".temperature-button").on("click",function(){
+  $("#temperature-button").on("click",function(){
     if (token == "F") {
-      $(".temperature-info").html(temperatureC + "°C");
-      $(".temperature-button").html("°F");
+      $("#main-temp").html(temperatureC + "°C");
+      $("#temperature-button").html("°F");
       token = "C";
     } else {
-      $(".temperature-info").html(temperatureF + "°F");
-      $(".temperature-button").html("°C");
+      $("#main-temp").html(temperatureF + "°F");
+      $("#temperature-button").html("°C");
       token = "F";
     }
   });
