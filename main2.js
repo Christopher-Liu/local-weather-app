814d9164638f4ed52dd954622555dafe4086b45d
christopher-liu <christopherbrianliu@gmail.com>
2018-01-25 17:23:44 +0000

    Added chained fetch call to pull weather data from pulled geolocation data

--- main2.js
+++ main2.js
@@ -1,9 +1,9 @@
 /* Beginning my re-write of this webapp to utilize JavaScript's Fetch API
 and removing jQuery as a dependency altogether. */
 
-let longi;
-let lati;
-let location;
+let userLongitude;
+let userLatitude;
+let userLocation;
 let temperatureF;
 let temperatureC;
 let unitsToken = "F";
@@ -15,8 +15,21 @@ let lowForecastC = [];
 
 
 // Fetch API test code. This can replace the jQuery AJAX call
-fetch('https://freegeoip.net/json/').then(res => {
-	return res.json();
-}).then( myJson => {
-	console.log(myJson)
-});
+fetch('https://freegeoip.net/json/')
+	.then( response => response.json())
+	.then( response => {
+		userLongitude = response.longitude;
+		userLatitude = response.latitude;
+		userLocation = response.location;
+
+		document.querySelector('#main-location').textContent = userLocation;
+
+		return fetch('https://api.wunderground.com/api/7965a61d5be76ab2/conditions/q/' + userLatitude + ',' + userLongitude + '.json');
+	})
+	.then( response => response.json())
+	.then( response => {
+		temperatureF = response.current_observation.temp_f;
+		temperatureC = response.current_observation.temp_c;
+
+		return fetch("https://api.wunderground.com/api/7965a61d5be76ab2/forecast/q/" + userLatitude + "," + userLongitude + ".json"),
+	})
