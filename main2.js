65d261dc900e4ec0ca133c165134973507461055
christopher-liu <christopherbrianliu@gmail.com>
2018-01-25 11:52:45 +0000

    Add main2.js file for rewritting the logic in pure JS and removing jQuery as a dependency

---
+++ main2.js
@@ -0,0 +1,14 @@
+/* Beginning my re-write of this webapp to utilize JavaScript's Fetch API
+and removing jQuery as a dependency altogether. */
+
+let longi;
+let lati;
+let location;
+let temperatureF;
+let temperatureC;
+let unitsToken = "F";
+
+let highForecastF = [];
+let lowForecastF = [];
+let highForecastC = [];
+let lowForecastC = [];
